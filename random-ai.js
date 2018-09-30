167829baf7f0a1b26f2efb8b9f083ed4b5e5fc19
raydson.ferreira <raydsonferreira@gmail.com>
2018-09-30 15:57:18 -0300

    uppando trabalho

---
+++ random-ai.js
@@ -0,0 +1,7 @@
+othello.registerAI({
+  findTheBestMove: function (gameTree) {
+    console.log(gameTree);
+    return gameTree.moves[Math.floor(Math.random() * gameTree.moves.length)];
+  }
+});
+// vim: expandtab softtabstop=2 shiftwidth=2 foldmethod=marker
